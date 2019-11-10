23463a6ba10ddf7dfdd3c70a379342a081364ff4
fb@frank-buss.de <fb@frank-buss.de>
2019-11-10 06:06:14 +0100

    initial version of the LiteX core with VGA terminal

---
+++ fpga/litex/platforms/max1000.py
@@ -0,0 +1,102 @@
+# This file is Copyright (c) 2019 Antti Lukats <antti.lukats@gmail.com>
+# This file is Copyright (c) 2014-2019 Florent Kermarrec <florent@enjoy-digital.fr>
+# License: BSD
+
+from litex.build.generic_platform import *
+from litex.build.altera import AlteraPlatform
+from litex.build.altera.programmer import USBBlaster
+
+# IOs ----------------------------------------------------------------------------------------------
+
+_io = [
+    ("clk12", 0, Pins("H6"), IOStandard("3.3-V LVTTL")),    # 12MHz clock
+    ("user_led", 0, Pins("C5"), IOStandard("3.3-V LVTTL")), # CONF Done, inverted polarity
+
+    ("sw", 0, Pins("E6"), IOStandard("3.3-V LVTTL")),
+    ("sw", 1, Pins("E7"), IOStandard("3.3-V LVTTL")), # nConfig
+
+    ("serial", 0,
+        Subsignal("tx", Pins("B4"), IOStandard("3.3-V LVTTL")),
+        Subsignal("rx", Pins("A4"), IOStandard("3.3-V LVTTL"))
+    ),
+
+    ("spiflash4x", 0,
+        Subsignal("cs_n", Pins("B3")),
+        Subsignal("clk", Pins("A3")),
+        Subsignal("dq", Pins("A2", "B2", "B9", "C4")),
+        IOStandard("3.3-V LVTTL")
+    ),
+    ("spiflash", 0,
+        Subsignal("cs_n", Pins("B3")),
+        Subsignal("clk", Pins("A3")),
+        Subsignal("mosi", Pins("A2")),
+        Subsignal("miso", Pins("B2")),
+        Subsignal("wp", Pins("B9")),
+        Subsignal("hold", Pins("C4")),
+        IOStandard("3.3-V LVTTL"),
+    ),
+
+    ("vga", 0,
+        Subsignal("red", Pins("K11", "J13", "J12", "L12", "N3", "N2")),
+        Subsignal("green", Pins("K2", "K1", "J2", "J1", "H4", "H5")),
+        Subsignal("blue", Pins("K10", "H8", "E4", "F1", "E3", "D1")),
+        Subsignal("hsync", Pins("C2")),
+        Subsignal("vsync", Pins("C1")),
+        IOStandard("3.3-V LVTTL"),
+    ),
+
+    ("sdram_clock", 0, Pins("M9"), IOStandard("3.3-V LVTTL")),
+    ("sdram", 0,
+        Subsignal("a", Pins("K6 M5 N5 J8 N10 M11 N9 L10 M13 N8 N4 M10")), #0, 1, ...
+        Subsignal("ba", Pins("N6 K8")),
+        Subsignal("cs_n", Pins("M4")),
+        Subsignal("cke", Pins("M8")),
+        Subsignal("ras_n", Pins("M7")),
+        Subsignal("cas_n", Pins("N7")),
+        Subsignal("we_n", Pins("K7")),
+        Subsignal("dq", Pins("D11 G10 F10 F9 E10 D9 G9 F8 F13 E12 E13 D12 C12 B12 B13 A12")),
+        Subsignal("dm", Pins("E9 F12")),
+        IOStandard("3.3-V LVTTL")
+    ),
+
+#    ("epcs", 0,
+#        Subsignal("data0", Pins("H2")),
+#        Subsignal("dclk", Pins("A3")),
+#        Subsignal("ncs0", Pins("B3")),
+#        Subsignal("asd0", Pins("C1")),
+#        IOStandard("3.3-V LVTTL")
+#    ),
+
+#    ("i2c", 0,
+#        Subsignal("sclk", Pins("F2")),
+#        Subsignal("sdat", Pins("F1")),
+#        IOStandard("3.3-V LVTTL")
+#    ),
+
+#    ("g_sensor", 0,
+#        Subsignal("cs_n", Pins("G5")),
+#        Subsignal("int", Pins("M2")),
+#        IOStandard("3.3-V LVTTL")
+#    ),
+
+    ("gpio_leds", 0,
+        Pins("D8 A8 A9 C9 A10 B10 A11 C10"),
+        IOStandard("3.3-V LVTTL")
+    ),
+
+]
+
+# Platform -----------------------------------------------------------------------------------------
+
+class Platform(AlteraPlatform):
+    default_clk_name = "clk12"
+    default_clk_period = 83
+
+    def __init__(self, device):
+        AlteraPlatform.__init__(self, device, _io)
+        self.add_platform_command("set_global_assignment -name FAMILY \"MAX 10\"")
+        self.add_platform_command("set_global_assignment -name ENABLE_CONFIGURATION_PINS OFF")
+        self.add_platform_command("set_global_assignment -name INTERNAL_FLASH_UPDATE_MODE \"SINGLE IMAGE WITH ERAM\"")
+
+    def create_programmer(self):
+        return USBBlaster()
---
+++ fpga/litex/targets/max1000.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python3
+
+# This file is Copyright (c) 2019 Antti Lukats <antti.lukats@gmail.com>
+# This file is Copyright (c) 2019 msloniewski <marcin.sloniewski@gmail.com>
+# License: BSD
+
+import argparse
+
+from migen import *
+
+from litex_boards.partner.platforms import max1000
+
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.soc_sdram import *
+
+from litex.soc.integration.builder import *
+
+from litedram.modules import MT48LC4M16
+from litedram.phy import GENSDRPHY
+
+from litex.soc.cores import gpio
+from litex.soc.cores.spi_flash import SpiFlash
+from litex.soc.interconnect.csr import *
+from litex.soc.interconnect import wishbone
+from terminal import Terminal
+
+class ClassicLed(Module):
+    def __init__(self, pads):
+        gpio.GPIOOut.__init__(self, pads)
+
+# CRG ----------------------------------------------------------------------------------------------
+class _CRG(Module):
+    def __init__(self, platform):
+        self.clock_domains.cd_sys = ClockDomain()
+        self.clock_domains.cd_sys_ps = ClockDomain()
+        self.clock_domains.cd_vga = ClockDomain()
+        self.clock_domains.cd_por = ClockDomain(reset_less=True)
+
+        # # #
+
+        self.cd_sys.clk.attr.add("keep")
+        self.cd_sys_ps.clk.attr.add("keep")
+        self.cd_por.clk.attr.add("keep")
+
+        # clock input always available
+        clk12 = platform.request("clk12")
+
+        # power on rst
+        rst_n = Signal()
+        self.sync.por += rst_n.eq(1)
+        self.comb += [
+            self.cd_por.clk.eq(clk12),
+            self.cd_sys.rst.eq(~rst_n),
+            self.cd_sys_ps.rst.eq(~rst_n)
+        ]
+
+        clk_outs = Signal(5)
+
+        self.comb += self.cd_sys.clk.eq(clk_outs[0]) # C0
+        self.comb += self.cd_sys_ps.clk.eq(clk_outs[1]) # C1
+        
+        # 25 MHz for 640x480. VESA standard is 25.175 MHz, but allows 0.5% deviation, close enough.
+        self.comb += self.cd_vga.clk.eq(clk_outs[2]) # C2
+
+        #
+        # PLL we need 2 clocks one system one for SDRAM phase shifter
+        # 
+
+        self.specials += \
+            Instance("ALTPLL",
+                p_BANDWIDTH_TYPE="AUTO",
+                p_CLK0_DIVIDE_BY=6,
+                p_CLK0_DUTY_CYCLE=50,
+                p_CLK0_MULTIPLY_BY=25,
+                p_CLK0_PHASE_SHIFT="0",
+                p_CLK1_DIVIDE_BY=6,
+                p_CLK1_DUTY_CYCLE=50,
+                p_CLK1_MULTIPLY_BY=25, 
+                p_CLK1_PHASE_SHIFT="-10000",
+                p_CLK2_DIVIDE_BY=12,
+                p_CLK2_DUTY_CYCLE=50,
+                p_CLK2_MULTIPLY_BY=25, 
+                p_CLK2_PHASE_SHIFT="0",
+                p_COMPENSATE_CLOCK="CLK0",
+                p_INCLK0_INPUT_FREQUENCY=83000,
+                p_INTENDED_DEVICE_FAMILY="MAX 10",
+                p_LPM_TYPE = "altpll",
+                p_OPERATION_MODE = "NORMAL",
+                i_INCLK=clk12,
+                o_CLK=clk_outs, # we have total max 5 Cx clocks
+                i_ARESET=~rst_n,
+                i_CLKENA=0x3f,
+                i_EXTCLKENA=0xf,
+                i_FBIN=1,
+                i_PFDENA=1,
+                i_PLLENA=1,
+            )
+        self.comb += platform.request("sdram_clock").eq(self.cd_sys_ps.clk)
+
+# BaseSoC ------------------------------------------------------------------------------------------
+
+class BaseSoC(SoCSDRAM):
+#class BaseSoC(SoCCore):
+    csr_peripherals = (
+        "leds"
+    )
+#    csr_map.update(SoCCore.csr_map, csr_peripherals)
+
+    def __init__(self, device, sys_clk_freq=int(50e6), **kwargs):
+        assert sys_clk_freq == int(50e6)
+
+        platform = max1000.Platform(device)
+
+#        SoCSDRAM.__init__(self, platform, clk_freq=sys_clk_freq,
+#                          integrated_rom_size=0x8000,
+#                          **kwargs)
+
+
+#    csr_map_update(SoCSDRAM.csr_map, csr_peripherals)
+
+        SoCSDRAM.__init__(self, platform, clk_freq=sys_clk_freq,
+            integrated_rom_size=0x6000,
+#            integrated_main_ram_size=0x4000,
+            **kwargs)
+ 
+        self.mem_map['spiflash'] = 0x20000000
+        spiflash_pads = platform.request('spiflash')
+        self.add_memory_region(
+            "spiflash", self.mem_map["spiflash"], 8*1024*1024)
+
+        self.submodules.spiflash = SpiFlash(
+                spiflash_pads,
+                dummy=8,
+                div=4,
+                endianness=self.cpu.endianness)
+        self.add_csr("spiflash")
+
+        #self.spiflash.add_clk_primitive("xc7");    
+
+        # 8 MB flash: W74M64FVSSIQ
+        self.add_constant("SPIFLASH_PAGE_SIZE", 256)
+        self.add_constant("SPIFLASH_SECTOR_SIZE", 4096)
+        self.add_constant("FLASH_BOOT_ADDRESS", self.mem_map['spiflash'])
+
+        # spi_flash.py supports max 16MB linear space
+        self.add_wb_slave(mem_decoder(self.mem_map["spiflash"]), self.spiflash.bus)
+
+        self.submodules.crg = crg = _CRG(platform)
+
+        # create VGA terminal
+        self.mem_map['terminal'] = 0x30000000
+        self.submodules.terminal = terminal = Terminal(crg.cd_vga.clk)
+        self.add_wb_slave(mem_decoder(self.mem_map["terminal"]), self.terminal.bus)
+        self.add_memory_region("terminal", self.mem_map["terminal"], 0x10000)
+
+        # connect VGA pins
+        vga = platform.request('vga', 0)
+        self.comb += [
+            vga.vsync.eq(terminal.vga_vsync),
+            vga.hsync.eq(terminal.vga_hsync),
+            vga.red.eq(terminal.red[2:8]),
+            vga.green.eq(terminal.green[2:8]),
+            vga.blue.eq(terminal.blue[2:8])
+        ]
+ 
+#        self.submodules.leds = ClassicLed(Cat(platform.request("user_led", i) for i in range(7)))
+        self.add_csr("leds", allow_user_defined=True)
+        self.submodules.leds = ClassicLed(platform.request("user_led", 0))
+
+#        self.add_csr("gpio_leds", allow_user_defined=True)
+        self.add_csr("gpio_leds", allow_user_defined=True)
+        self.submodules.gpio_leds = gpio.GPIOOut(platform.request("gpio_leds"))
+
+
+# use micron device as winbond and ISSI not available
+
+        if not self.integrated_main_ram_size:
+            self.submodules.sdrphy = GENSDRPHY(platform.request("sdram"))
+            sdram_module = MT48LC4M16(self.clk_freq, "1:1")
+            self.register_sdram(self.sdrphy,
+                                sdram_module.geom_settings,
+                                sdram_module.timing_settings)
+
+# Build --------------------------------------------------------------------------------------------
+
+def testbench(dut):
+    for i in range(100):
+        yield dut.clk.eq(i & 1)
+        yield
+        print("text_addr:{} text_addr_start:{}".format((yield dut.terminal.text_addr_start), (yield dut.terminal.text_addr_start)))
+
+        # current line within a character, 0 to 15
+        #self.fline = fline = Signal(4)
+
+        # current x position within a character, 0 to 7
+        #self.fx = fx = Signal(3)
+
+def main():
+    parser = argparse.ArgumentParser(description="LiteX SoC on MAX1000")
+    builder_args(parser)
+    parser.add_argument("device", choices=['8', '16'], help='Cyclone device: "8" for 10M08SAU169C8G or "16" for 10M16SAU169C8G')
+
+    soc_sdram_args(parser)
+#    soc_core_args(parser)
+
+    args = parser.parse_args()
+
+    if args.device == '16':
+        device = '10M16SAU169C8G'
+    else:
+        device = '10M08SAU169C8G'
+    soc = BaseSoC(device, **soc_sdram_argdict(args))
+
+#    cls = BaseSoC
+#    soc = cls(**soc_core_argdict(args))
+
+    # run_simulation(soc, testbench(soc))
+
+    Builder(soc, **builder_argdict(args)).build()
+
+if __name__ == "__main__":
+    main()
